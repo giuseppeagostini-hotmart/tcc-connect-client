571b82ba43af924d45831617fee239fea630129c
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-08-22 21:25:36 -0300

    Updates

--- assets/NotFound-92330f8e.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as o}from"./index-190492c6.js";function r(){return n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Not Found"}),n.jsx(o,{to:"/home",children:"GO HOME"})]})}export{r as default};
