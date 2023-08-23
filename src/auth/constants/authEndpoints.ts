129dbab5ba55d12e30b2e2333bcdd4557d6892ee
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-08-23 18:42:59 -0300

    fix: update endpoint

--- src/auth/constants/authEndpoints.ts
+++ src/auth/constants/authEndpoints.ts
@@ -1,13 +1,19 @@
 import RoutesPaths from '@src/core/routes/constants'
 
+const prodURL = 'https://api-tcc-connect.onrender.com'
+
+const baseURL = import.meta.env.DEV
+  ? import.meta.env.VITE_REACT_APP_DOMAIN_API_TCC_CONNECT
+  : prodURL
+
 const AuthUtil = {
   logged: '/isLogged',
   logout: '/logout'
 }
 
 export const AuthEndpoints = {
-  login: `${import.meta.env.VITE_REACT_APP_DOMAIN_API_TCC_CONNECT}${RoutesPaths.Login}`,
-  signup: `${import.meta.env.VITE_REACT_APP_DOMAIN_API_TCC_CONNECT}${RoutesPaths.Signup}`,
-  getIsLogged: `${import.meta.env.VITE_REACT_APP_DOMAIN_API_TCC_CONNECT}${AuthUtil.logged}`,
-  getLogout: `${import.meta.env.VITE_REACT_APP_DOMAIN_API_TCC_CONNECT}${AuthUtil.logout}`
+  login: `${baseURL}${RoutesPaths.Login}`,
+  signup: `${baseURL}${RoutesPaths.Signup}`,
+  getIsLogged: `${baseURL}${AuthUtil.logged}`,
+  getLogout: `${baseURL}${AuthUtil.logout}`
 }
