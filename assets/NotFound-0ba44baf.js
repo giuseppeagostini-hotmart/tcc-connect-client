e771716e25f44c43127500e8376aebf0c5d1fe1c
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-08-22 21:48:52 -0300

    Updates

---
+++ assets/NotFound-0ba44baf.js
@@ -0,0 +1,1 @@
+import{j as n,L as o}from"./index-52af962b.js";function r(){return n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Not Found"}),n.jsx(o,{to:"/home",children:"GO HOME"})]})}export{r as default};
--- assets/NotFound-896960d1.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as o}from"./index-c5ab8c7d.js";function r(){return n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Not Found"}),n.jsx(o,{to:"/home",children:"GO HOME"})]})}export{r as default};
