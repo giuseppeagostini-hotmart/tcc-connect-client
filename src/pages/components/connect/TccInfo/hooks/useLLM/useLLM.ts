6f86858f688ff111fe29c966175bd300a6377030
giuseppefantoni-hotmart <giuseppe.agostini@hotmart.com>
2023-12-12 17:31:13 -0300

    useLLM

--- src/pages/components/connect/TccInfo/hooks/useLLM/useLLM.ts
+++ src/pages/components/connect/TccInfo/hooks/useLLM/useLLM.ts
@@ -5,7 +5,7 @@ import { OpenAI } from 'langchain/llms/openai'
 const useLLM = (key: string) =>
   useMemo(() => {
     return new OpenAI({
-      temperature: 0.3,
+      temperature: 0.6,
       openAIApiKey: key,
       modelName: 'gpt-3.5-turbo'
     })
